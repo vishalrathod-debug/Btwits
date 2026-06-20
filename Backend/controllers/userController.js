aefef393c44f435e0b6b086774ea379200ad2d80
Vishal Rathod <1155defender@gmail.com>
2026-06-21 00:16:10 +0530

    basic setup

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,50 @@
+const User = require("../models/user.model");
+
+const registerUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // 1. Basic validation
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
+
+        // 2. Check if user already exists
+        const existingUser = await User.findOne({
+            $or: [{ email }, { username }]
+        });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        // 3. Create user
+        const user = await User.create({
+            username,
+            email,
+            password   // ⚠️ plain for now (you'll hash later)
+        });
+
+        // 4. Send response (DON'T send password)
+        res.status(201).json({
+            message: "User registered successfully",
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        console.log("Register Error:", error);
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+};
+
+module.exports = { registerUser };
---
+++ Backend/index.js
@@ -0,0 +1,31 @@
+console.log("App starting...");
+require('dotenv').config();
+const express = require("express");
+const cors = require('cors');
+const dbConnection = require('./config/db');
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+// start server function
+const startServer = async () => {
+    try {
+        await dbConnection();
+
+        app.listen(process.env.PORT || 3000, () => {
+            console.log("Server is running on:", process.env.PORT);
+        });
+
+    } catch (error) {
+        console.log("Error occurred:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
